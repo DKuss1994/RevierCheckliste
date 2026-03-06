13a5aed0c06c091bba03a060c69427da49974861
DKuss1994 <kussdennis@web.de>
2026-03-06 19:29:21 +0100

    test: add weekday restriction test

--- src/main/java/com/example/RevierCheckliste/domain/service/PatrolService.java
+++ src/main/java/com/example/RevierCheckliste/domain/service/PatrolService.java
@@ -1,4 +1,21 @@
 package com.example.RevierCheckliste.domain.service;
 
+import java.time.LocalDate;
+
 public class PatrolService {
+    private LocalDate localDate;
+    public PatrolService(){
+
+    }
+    public PatrolService(LocalDate localDate){
+        this.localDate = localDate;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
 }
