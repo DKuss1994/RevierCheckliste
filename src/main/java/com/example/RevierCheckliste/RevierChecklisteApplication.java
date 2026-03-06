4f9350bb3c84122f5714f3c974b6be16484018db
DKuss1994 <kussdennis@web.de>
2026-03-06 11:34:54 +0100

    Initial commit

---
+++ src/main/java/com/example/RevierCheckliste/RevierChecklisteApplication.java
@@ -0,0 +1,13 @@
+package com.example.RevierCheckliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevierChecklisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevierChecklisteApplication.class, args);
+	}
+
+}
