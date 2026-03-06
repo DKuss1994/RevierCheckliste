20345d6ce11d78351d1205d6f3130d27497e4b6a
DKuss1994 <kussdennis@web.de>
2026-03-06 19:37:11 +0100

    refactor: initialize allowed weekdays to avoid null state

--- src/main/java/com/example/RevierCheckliste/domain/site/StandardConfiguration.java
+++ src/main/java/com/example/RevierCheckliste/domain/site/StandardConfiguration.java
@@ -1,11 +1,12 @@
 package com.example.RevierCheckliste.domain.site;
 
 import java.time.DayOfWeek;
+import java.util.HashSet;
 import java.util.Set;
 
 public class StandardConfiguration {
     private int controlCount;
-    private Set<DayOfWeek> allowedWeekdays;
+    private Set<DayOfWeek> allowedWeekdays = new HashSet<DayOfWeek>();
     public StandardConfiguration(int controlCount){
         this.controlCount = controlCount;
     }
