f0c7e30ffe275957940c834ad30877931a62f45e
DKuss1994 <kussdennis@web.de>
2026-03-12 12:56:06 +0100

    feat: Add OverrideRule class  and validation

--- src/main/java/com/example/RevierCheckliste/domain/Rules/OverrideRule.java
+++ src/main/java/com/example/RevierCheckliste/domain/Rules/OverrideRule.java
@@ -3,13 +3,14 @@ package com.example.RevierCheckliste.domain.Rules;
 import com.example.RevierCheckliste.domain.StandardConfiguration;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class OverrideRule extends Rule{
     private final StandardConfiguration configuration;
 
     public OverrideRule(LocalDate startDay, LocalDate endDay, StandardConfiguration config) {
         super(startDay, endDay);
-        this.configuration = config;
+        this.configuration = Objects.requireNonNull(config,"OverrideRule configuration must not be null");
     }
 
     public StandardConfiguration getConfiguration() {
