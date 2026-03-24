df083a235c7484846bf27d5a927fca20bfabeb6e
DKuss1994 <kussdennis@web.de>
2026-03-24 13:09:16 +0100

    chore: add Spring Boot main application class

---
+++ src/main/java/com/example/securitydispatch/SecuritydispatchApplication.java
@@ -0,0 +1,9 @@
+package com.example.securitydispatch;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SecuritydispatchApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SecuritydispatchApplication.class,args);
+    }
+}
