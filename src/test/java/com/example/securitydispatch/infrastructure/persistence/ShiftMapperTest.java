f62033a79535a6bd9a6c2c067421b17e25d2c377
DKuss1994 <kussdennis@web.de>
2026-03-25 12:38:28 +0100

    test: add ShiftEntity tests

---
+++ src/test/java/com/example/securitydispatch/infrastructure/persistence/ShiftMapperTest.java
@@ -0,0 +1,27 @@
+package com.example.securitydispatch.infrastructure.persistence;
+import com.example.securitydispatch.domain.Driver;
+import com.example.securitydispatch.domain.Shift;
+import com.example.securitydispatch.domain.Zone;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ShiftMapperTest {
+    @Test
+    void shouldMapDriverToDomainAndBack(){
+        Driver driver = new Driver(1L,"Max","Muster");
+        Zone zone = new Zone(1L,"Zone 1");
+        LocalDate startDay = LocalDate.of(2026,3,3);
+        LocalTime startTime = LocalTime.of(22,0);
+        LocalTime endTime = LocalTime.of(6,0);
+
+        Shift shift = new Shift(1L,driver,zone,startDay,startTime,endTime);
+        ShiftEntity entity = ShiftMapper.toEntity(shift);
+        Shift toDomain = ShiftMapper.toDomain(entity);
+        assertThat(toDomain.getId()).isEqualTo(shift.getId());
+        assertThat(toDomain.getDeploymentDate()).isEqualTo(startDay);
+    }
+}
