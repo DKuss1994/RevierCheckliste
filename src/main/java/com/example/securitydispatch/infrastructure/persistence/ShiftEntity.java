ccdbba349b0b1c44ff6f4dce547223ab3c5b7b2c
DKuss1994 <kussdennis@web.de>
2026-03-25 12:38:05 +0100

    feat: add ShiftEntity

---
+++ src/main/java/com/example/securitydispatch/infrastructure/persistence/ShiftEntity.java
@@ -0,0 +1,56 @@
+package com.example.securitydispatch.infrastructure.persistence;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+@Entity
+@Table(name = "shifts")
+public class ShiftEntity {
+    @Id
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "driver_id")
+    private DriverEntity driver;
+
+    @ManyToOne
+    @JoinColumn(name = "zone_id")
+    private ZoneEntity zone;
+
+    private LocalDate deploymentDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+protected ShiftEntity(){}
+    public ShiftEntity(long id,DriverEntity driver, ZoneEntity zone,LocalDate deploymentDate
+    ,LocalTime startTime,LocalTime endTime){
+    this.id = id;
+    this.driver = driver;
+    this.zone = zone;
+    this.deploymentDate = deploymentDate;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public DriverEntity getDriver() {
+        return driver;
+    }
+
+    public ZoneEntity getZone() {
+        return zone;
+    }
+
+    public LocalDate getDeploymentDate() {
+        return deploymentDate;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+}
