3956c7e3886bd60d668881e9b77db916787d5c28
DKuss1994 <kussdennis@web.de>
2026-03-25 11:49:47 +0100

    test: SecurityObjectMapper test domain to entity and back

---
+++ src/main/java/com/example/securitydispatch/infrastructure/persistence/SecurityObjectMapper.java
@@ -0,0 +1,13 @@
+package com.example.securitydispatch.infrastructure.persistence;
+
+import com.example.securitydispatch.domain.SecurityObject;
+
+public class SecurityObjectMapper {
+    public static SecurityObjectEntity toEntity(SecurityObject securityObject) {
+        return null;
+    }
+
+    public static SecurityObject toDomain(SecurityObjectEntity entity) {
+        return null;
+    }
+}
---
+++ src/test/java/com/example/securitydispatch/infrastructure/persistence/SecurityObjectMapperTest.java
@@ -0,0 +1,34 @@
+package com.example.securitydispatch.infrastructure.persistence;
+import com.example.securitydispatch.domain.*;
+import org.junit.jupiter.api.Test;
+import java.time.DayOfWeek;
+import java.util.Set;
+import static org.assertj.core.api.Assertions.assertThat;
+public class SecurityObjectMapperTest {
+
+    private final Zone zone = new Zone(1L, "Zone 1");
+    private final Address address = new Address("Musterstraße 1", "Berlin", "10115");
+    private final StandardConfiguration config = new StandardConfiguration.Builder()
+            .inspectionCount(2)
+            .inspectionDays(Set.of(DayOfWeek.MONDAY, DayOfWeek.FRIDAY))
+            .build();
+    private final SecurityObject securityObject = new SecurityObject(
+            1L,
+            "Object A",
+            zone,
+            address,
+            config
+    );
+    @Test
+    void shouldMapSecurityObjectToDomainAndBack(){
+        SecurityObjectEntity entity = SecurityObjectMapper.toEntity(securityObject);
+        SecurityObject mapped = SecurityObjectMapper.toDomain(entity);
+        assertThat(mapped).isNotNull();
+        assertThat(mapped.getId()).isEqualTo(securityObject.getId());
+        assertThat(mapped.getName()).isEqualTo(securityObject.getName());
+        assertThat(mapped.getAddress()).isEqualTo(securityObject.getAddress());
+        assertThat(mapped.getZone()).isEqualTo(securityObject.getZone());
+        assertThat(mapped.getStandardConfiguration()).isEqualTo(securityObject.getStandardConfiguration());
+
+    }
+}
