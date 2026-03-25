c2f12c36c4db6aa1a9425f644a14369adaadca1b
DKuss1994 <kussdennis@web.de>
2026-03-25 15:40:34 +0100

    feat: add ShiftEntity

---
+++ src/main/java/com/example/securitydispatch/infrastructure/persistence/ShiftMapper.java
@@ -0,0 +1,23 @@
+package com.example.securitydispatch.infrastructure.persistence;
+
+import com.example.securitydispatch.domain.Driver;
+import com.example.securitydispatch.domain.Shift;
+import com.example.securitydispatch.domain.Zone;
+
+public class ShiftMapper {
+    public static ShiftEntity toEntity(Shift shift) {
+        DriverEntity driver = DriverMapper.toEntity(shift.getDriver());
+        ZoneEntity zone = ZoneMapper.toEntity(shift.getZone());
+        return new ShiftEntity
+                (shift.getId(), driver, zone, shift.getDeploymentDate(),
+                        shift.getStartTime(), shift.getEndTime());
+    }
+
+    public static Shift toDomain(ShiftEntity entity) {
+        Driver driver = DriverMapper.toDomain(entity.getDriver());
+        Zone zone = ZoneMapper.toDomain(entity.getZone());
+        return new Shift(entity.getId(),driver,zone,
+                entity.getDeploymentDate(),entity.getStartTime(),
+                entity.getEndTime());
+    }
+}
