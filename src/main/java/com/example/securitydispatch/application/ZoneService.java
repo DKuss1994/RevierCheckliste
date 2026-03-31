45650c8d259639a12dbc65cd90ed1ef70026fe49
DKuss1994 <kussdennis@web.de>
2026-03-31 20:01:57 +0200

    feat: ZoneService for read and create

--- src/main/java/com/example/securitydispatch/application/ZoneService.java
+++ src/main/java/com/example/securitydispatch/application/ZoneService.java
@@ -1,20 +1,36 @@
 package com.example.securitydispatch.application;
 
 import com.example.securitydispatch.domain.Zone;
+import com.example.securitydispatch.infrastructure.persistence.ZoneEntity;
+import com.example.securitydispatch.infrastructure.persistence.ZoneMapper;
+import com.example.securitydispatch.infrastructure.persistence.ZoneRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
 import java.util.List;
 
 public class ZoneService {
+    private final ZoneRepository zoneRepository;
+
+    public ZoneService(ZoneRepository zoneRepository) {
+        this.zoneRepository = zoneRepository;
+    }
+
 
     public Zone create(long id, String name) {
-        return null;
+        ZoneEntity entity = new ZoneEntity(id,name);
+        return ZoneMapper.toDomain(zoneRepository.save(entity));
     }
 
     public Zone findById(long id) {
-        return null;
+        return zoneRepository.findById(id).map(
+                ZoneMapper::toDomain
+        ).orElseThrow(()->new IllegalArgumentException("Zone not found: "+id));
     }
 
     public List<Zone>   findAll() {
-    return null;
+    return zoneRepository.findAll()
+            .stream().map(ZoneMapper::toDomain)
+            .toList();
     }
 }
