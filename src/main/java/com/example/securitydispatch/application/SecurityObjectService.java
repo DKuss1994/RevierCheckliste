338f8756352f44faef0a5c67788227109e234ba3
DKuss1994 <kussdennis@web.de>
2026-04-01 22:04:42 +0200

    test: add SecurityObjectService test  with CRUD operations

---
+++ src/main/java/com/example/securitydispatch/application/SecurityObjectService.java
@@ -0,0 +1,54 @@
+package com.example.securitydispatch.application;
+
+import com.example.securitydispatch.domain.SecurityObject;
+import com.example.securitydispatch.domain.Zone;
+import com.example.securitydispatch.infrastructure.persistence.*;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+
+import java.util.List;
+
+public class SecurityObjectService {
+    private final ZoneRepository zoneRepository;
+    private final SecurityObjectRepository securityObjectRepository;
+
+    public SecurityObjectService(ZoneRepository zoneRepository, SecurityObjectRepository securityObjectRepository) {
+        this.zoneRepository = zoneRepository;
+        this.securityObjectRepository = securityObjectRepository;
+    }
+
+    public SecurityObject create(long objectId, String name, long zoneId, AddressEmbeddable address, StandardConfigurationEmbeddable standardConfiguration) {
+        ZoneEntity zoneEntity = zoneRepository.findById(zoneId)
+                .orElseThrow(()->new IllegalArgumentException("Zone not found: " + zoneId));
+
+        SecurityObjectEntity objectEntity = new SecurityObjectEntity(objectId,name,zoneEntity,address,standardConfiguration);
+        return SecurityObjectMapper.toDomain(securityObjectRepository.save(objectEntity));
+    }
+    public SecurityObject findById(long id){
+        return securityObjectRepository.findById(id).map(
+                SecurityObjectMapper::toDomain
+        ).orElseThrow(() -> new IllegalArgumentException("SecurityObject not found: " + id));
+    }
+
+    public List<SecurityObject> findAll() {
+        return securityObjectRepository.findAll().stream()
+                .map(SecurityObjectMapper::toDomain).toList();
+
+    }
+
+
+    public SecurityObject update(long id, String name, long zoneId, AddressEmbeddable address, StandardConfigurationEmbeddable standardConfiguration) {
+        SecurityObjectEntity oldSecurity = securityObjectRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("SecurityObject not found: "+ id));
+        ZoneEntity zoneEntity = zoneRepository.findById(zoneId)
+                .orElseThrow(()->new IllegalArgumentException("Zone not found: " + zoneId));
+        SecurityObjectEntity securityObject = new SecurityObjectEntity(
+                id,name,zoneEntity,address,standardConfiguration
+        );
+        return SecurityObjectMapper.toDomain(securityObjectRepository.save(securityObject));
+    }
+
+    public void delete(long id) {
+        securityObjectRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("SecurityObject not found: "+ id));
+        securityObjectRepository.deleteById(id);
+    }
+}
