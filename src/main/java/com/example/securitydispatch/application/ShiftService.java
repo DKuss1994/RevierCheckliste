2098c2f9ce49ad1ddeb274a30d1038805352fc84
DKuss1994 <kussdennis@web.de>
2026-04-02 10:50:48 +0200

    feat: add ShiftService with CRUD operations

--- src/main/java/com/example/securitydispatch/application/ShiftService.java
+++ src/main/java/com/example/securitydispatch/application/ShiftService.java
@@ -1,27 +1,47 @@
 package com.example.securitydispatch.application;
 
 import com.example.securitydispatch.domain.Shift;
-import com.example.securitydispatch.infrastructure.persistence.DriverEntity;
-import com.example.securitydispatch.infrastructure.persistence.ZoneEntity;
+import com.example.securitydispatch.infrastructure.persistence.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 public class ShiftService {
+    private final DriverRepository driverRepository;
+    private final ZoneRepository zoneRepository;
+    private final ShiftRepository shiftRepository;
+
+    public ShiftService(DriverRepository driverRepository, ZoneRepository zoneRepository, ShiftRepository shiftRepository) {
+        this.driverRepository = driverRepository;
+        this.zoneRepository = zoneRepository;
+        this.shiftRepository = shiftRepository;
+    }
+
     public Shift create(long id, long driverId, long zoneId, LocalDate deploymentDate, LocalTime startTime, LocalTime endTime) {
-        return null;
+        DriverEntity driverEntity = driverRepository.findById(driverId)
+                .orElseThrow(() -> new IllegalArgumentException("Driver not found: " + driverId));
+        ZoneEntity zoneEntity = zoneRepository.findById(driverId)
+                .orElseThrow(() -> new IllegalArgumentException("Zone not found: " + zoneId));
+        ShiftEntity shiftEntity = new ShiftEntity(id, driverEntity, zoneEntity, deploymentDate, startTime, endTime);
+
+        return ShiftMapper.toDomain(shiftRepository.save(shiftEntity));
     }
 
     public Shift findById(long id) {
-        return null;
+        return shiftRepository.findById(id).map(
+                ShiftMapper::toDomain
+        ).orElseThrow(() -> new IllegalArgumentException("Shift not found: " + id));
     }
 
     public List<Shift> findAll() {
-        return null;
+        return shiftRepository.findAll().stream()
+                .map(ShiftMapper::toDomain).toList();
     }
 
     public void delete(long id) {
-
+        shiftRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Shift not found: " + id));
+        shiftRepository.deleteById(id);
     }
 }
+
