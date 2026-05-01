7e6c249d084f4c837e024a2c686a5aead8f68676
DKuss1994 <kussdennis@web.de>
2026-05-02 01:39:22 +0200

    feat: add dashboard with Bootstrap cards and navigation links

---
+++ src/main/java/com/example/securitydispatch/infrastructure/web/DashboardController.java
@@ -0,0 +1,12 @@
+package com.example.securitydispatch.infrastructure.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class DashboardController {
+    @GetMapping("/")
+    public String dashboard(){
+        return "index";
+    }
+}
